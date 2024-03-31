d82d33d643d4a1f37bf50944787ab3b320c1dca2
Saad Darbari <saaddarbari318@gmail.com>
2024-04-01 03:23:37 +0500

    Push my code on Github

---
+++ main.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+
+while(condition)
+{
+ let addtask = await inquirer.prompt(
+    [
+         {
+          name:"todo",
+          type:"input",
+          message:"what would you like to do?"
+         },
+         {
+          name:"addmore",
+          type:"confirm",
+          message:"Do you Want to Add more?",
+          default:"false"
+         }
+    ]
+ );
+ todos.push(addtask.todo);
+ condition = addtask.addmore
+ console.log(todos);
+};
+let clearList = await inquirer.prompt([
+    {
+        name: "clear",
+        type: "confirm",
+        message:"Do you want to clear your ToDo list?",
+        default: false,
+    },
+]);
+if (clearList.clear) {
+    todos = [];
+    console.log("\n--- To-Do List Is clear---");
+}
+else {
+    console.log("\n--- ToDo list not cleared! ---");
+}
+console.log("Final ToDo List:");
+console.log(todos);
